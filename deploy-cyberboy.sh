98d04cb46b0a611935f859c5ef5fceef9be7cefd
root <root@ip-172-31-0-144.ap-south-1.compute.internal>
2020-08-02 14:04:47 +0530

    first commit

---
+++ deploy-cyberboy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# [Author] Eric Abraham Kalloor
+#          Automated Cyberboy deployment
+# ------------------------------------------------------------------
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+cd /var/www/cyberboy/
+
+#git stash and pull
+echo 'pulling code'
+git stash
+git pull
+
+#activate venv
+echo 'Activating virtual environment'
+source venv/bin/activate
+
+#collect static
+echo 'Collecting Static Files'
+python manage.py collectstatic
+
+#make migrations and migrate
+echo 'Running Migraions'
+python manage.py makemigrations
+python manage.py migrate
+
+#deactivate vent
+echo 'Deactivating Virtual Environment'
+deactivate
+
+#remove existing staticfile folder and rename static folder
+echo 'Renaming Static Directory'
+rm -rf staticfiles/
+mv static/ staticfiles/
+
+#change ownership and permission of directory
+echo 'Changing Permission and Ownership'
+chown -R www-data:www-data .
+chmod -R 777 .
+
+#exit
+echo 'Deploy Completed'
